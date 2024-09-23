27147421a87032fc80fbbd747c09ef474efc8750
mohammed_A <modybam2000@gmail.com>
2024-09-23 16:06:11 +0300

    finishing basics of tasks

---
+++ tasks.py
@@ -0,0 +1,8 @@
+new_tasks=[
+    {'task':'quran' ,'completed': True},
+    {'task':'salah' ,'completed': True},
+    {'task': 'study','completed': False},
+    {'task': 'exercise','completed': True},
+    {'task': 'sleep','completed': False},
+    {'task': 'visit hamada','completed': True},
+]
